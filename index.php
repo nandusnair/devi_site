9e7524e5d69aa4ca5f258a599a48cf319112f9f3
nandusnair <nandusnair92@gmail.com>
2021-05-16 09:32:03 +0530

    index.html changed to index.php

--- index.php
+++ index.php
@@ -20,11 +20,11 @@
       <header>
         <div class="pp-header">
           <nav class="navbar navbar-expand-lg navbar-light">
-            <div class="container"><a href="index.html"><img src="images/favicon.png" alt="Logo"></a><h2><b>Devika Manoj</b></h2>
+            <div class="container"><a href="index.php"><img src="images/favicon.png" alt="Logo"></a><h2><b>Devika Manoj</b></h2>
               <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
               <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <ul class="navbar-nav ml-auto">
-                  <li class="nav-item active"><a class="nav-link" href="index.html">Home</a>
+                  <li class="nav-item active"><a class="nav-link" href="index.php">Home</a>
                   </li>
                   <li class="nav-item"><a class="nav-link" href="about.html">About</a>
                   </li>
